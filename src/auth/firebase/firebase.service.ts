60b058c88890136e5afcbff12f6834c8cf8711e5
Adrian Campana <adrianphcampana12@gmail.com>
2024-10-13 20:22:11 -0300

    :sparkles: feat: start the deployment

--- src/auth/firebase/firebase.service.ts
+++ src/auth/firebase/firebase.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
-import * as serviceAccount from '../../../firebaseadmin.json';
 
 @Injectable()
 export class FirebaseService {
   private app: admin.app.App;
 
   constructor() {
+    const firebaseConfigBase64 = process.env.FIREBASE_CONFIG_BASE64;
+    if (!firebaseConfigBase64) {
+      throw new Error('FIREBASE_CONFIG_BASE64 environment variable is missing');
+    }
+
+    const serviceAccount = JSON.parse(
+      Buffer.from(firebaseConfigBase64, 'base64').toString('utf-8'),
+    );
+
     if (!admin.apps.length) {
       this.app = admin.initializeApp({
-        credential: admin.credential.cert(
-          serviceAccount as admin.ServiceAccount,
-        ),
+        credential: admin.credential.cert(serviceAccount),
         databaseURL: 'https://qrious-51310-default-rtdb.firebaseio.com/',
       });
     } else {
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { rabbitmqConfig } from '../rabbitmq.options';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, rabbitmqConfig);
   await app.listen();
