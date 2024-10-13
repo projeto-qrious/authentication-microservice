af776fe2ecc1c1542d3173e61e56a717b0bc5be9
Adrian Campana <adrianphcampana12@gmail.com>
2024-10-13 20:50:25 -0300

    :bug: fix: fix rabbitmq options

--- rabbitmq.options.ts
+++ rabbitmq.options.ts
@@ -3,7 +3,7 @@ import { RmqOptions, Transport } from '@nestjs/microservices';
 export const rabbitmqConfig = {
   transport: Transport.RMQ,
   options: {
-    urls: ['amqp://localhost:5672'],
+    urls: ['amqp://rabbitmq-3-management-pr9r.onrender.com:5672'],
     queue: 'auth_queue',
     queueOptions: {
       durable: true,
