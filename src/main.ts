913512118fae1284373a5b5f6e4fd5fe51db84c9
Adrian Campana <adrianphcampana12@gmail.com>
2024-10-13 21:54:54 -0300

    :bug: fix deploy

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { rabbitmqConfig } from '../rabbitmq.options';
-import { Transport } from '@nestjs/microservices';
+
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  // microserviço RabbitMQ
-  const app = await NestFactory.createMicroservice(AppModule, rabbitmqConfig);
-  await app.listen();
+  // Cria o aplicativo HTTP
+  const app = await NestFactory.create(AppModule);
 
-  // Adicione um servidor HTTP básico apenas para o Render
-  const httpApp = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
-  await httpApp.listen(port, () => {
-    console.log(`HTTP server running on port ${port}`);
-  });
+  // Conecta o microserviço RabbitMQ
+  app.connectMicroservice(rabbitmqConfig);
+
+  // Inicia o microserviço
+  await app.startAllMicroservices();
+
+  // Faz o aplicativo HTTP escutar na porta especificada ou na porta padrão 3000
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
