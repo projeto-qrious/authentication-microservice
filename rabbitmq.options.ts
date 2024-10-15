7a4a0f70acb048aafbd4db7865305a464c8732fd
Adrian Campana <adrianphcampana12@gmail.com>
2024-10-15 20:21:36 -0300

    fix: update RabbitMQ URL configuration

--- rabbitmq.options.ts
+++ rabbitmq.options.ts
@@ -3,7 +3,7 @@ import { RmqOptions, Transport } from '@nestjs/microservices';
 export const rabbitmqConfig = {
   transport: Transport.RMQ,
   options: {
-    urls: ['amqp://guest:guest@rabbitmq-3-management-pr9r.onrender.com:5672'],
+    urls: ['amqp://guest:guest@rabbitmq-3-management-xk33.onrender.com:5672'],
     queue: 'auth_queue',
     queueOptions: {
       durable: true,
