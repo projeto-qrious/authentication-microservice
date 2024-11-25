7d2ad9b024c3e2c322021e2432ad6194d1a8eac6
Adrian Campana <adrianphcampana12@gmail.com>
2024-11-25 11:09:26 -0300

    fix: replace HttpException with RpcException for error handling in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { MessagePattern, Payload, RpcException } from '@nestjs/microservices';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -17,13 +17,7 @@ export class AuthController {
         message: 'Usuário registrado com sucesso',
       };
     } catch (error) {
-      throw new HttpException(
-        {
-          status: HttpStatus.BAD_REQUEST,
-          error: error.message,
-        },
-        HttpStatus.BAD_REQUEST,
-      );
+      throw new RpcException(error.message);
     }
   }
 
